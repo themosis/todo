fa3b0275c3cdd6617dc17dc3d2c6e83731348357
Julien <julien@jlambe.be>
2014-12-12 19:06:22 +0100

    Initial commit.

---
+++ config/environment.php
@@ -0,0 +1,11 @@
+<?php
+
+/*----------------------------------------------------*/
+// Define your environments
+/*----------------------------------------------------*/
+return array(
+
+    'local'         => 'Home-HP',
+    'production'    => 'your-production-hostname'
+
+);
