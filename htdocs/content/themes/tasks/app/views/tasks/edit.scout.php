110ff47bfef488cbb08e1f783e6fc298eacf18dd
Julien <julien@jlambe.be>
2014-12-22 16:36:06 +0100

    Edit task screen styling.

--- htdocs/content/themes/tasks/app/views/tasks/edit.scout.php
+++ htdocs/content/themes/tasks/app/views/tasks/edit.scout.php
@@ -1,17 +1,45 @@
-<h2>Edit:</h2>
+@extends('layouts.tasks')
 
-@if(isset($error))
-    <p>{{ $error }}</p>
-@endif
+@section('toolbar')
+    <a class="back-to-tasks" href="{{ home_url('tasks') }}">< Back</a>
+@stop
 
-<p>
-    <a href="{{ home_url('tasks') }}">Back to tasks list</a>
-</p>
+@section('toolbar_title')
+    <h3>Edit task</h3>
+@stop
 
-{{ Form::open() }}
+@section('messages')
+    @if(isset($error))
+        <p>{{{ $error }}}</p>
+    @endif
+@stop
 
-<p>{{ Form::text('task', $task->post_title) }} - Due date ? - {{ Form::text('schedule', Meta::get($task->ID, 'task_due_date')) }}</p>
+@section('content')
 
-<p>{{ Form::submit('edit', 'Edit task') }}</p>
+    {{ Form::open() }}
 
-{{ Form::close() }}
+    <div class="task">
+        <div class="clearfix">
+            <div class="task__date--set left">
+                <?php
+                    $date = Meta::get($task->ID, 'task_due_date');
+                ?>
+                {{ Form::hidden('schedule', $date) }}
+
+                @if(empty($date))
+                    {{ Form::button('toggleDate', 'Pick a date', array('class' => 'due-date')) }}
+                @else
+                    <p>{{{ $date }}}</p>
+                @endif
+            </div>
+            <div class="task__name--set left">
+                {{ Form::text('task', $task->post_title, array('placeholder' => 'Type your task here...')) }}
+            </div>
+        </div>
+    </div>
+
+    <p class="submit">{{ Form::submit('edit', 'Edit task', array('class' => 'create-task')) }}</p>
+
+    {{ Form::close() }}
+
+@stop
