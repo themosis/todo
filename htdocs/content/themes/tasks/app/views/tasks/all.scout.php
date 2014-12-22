9ce628b83ab3e8a463d25e728e892f8df093cf96
Julien <julien@jlambe.be>
2014-12-22 12:03:49 +0100

    Toolbar and message default styling.

--- htdocs/content/themes/tasks/app/views/tasks/all.scout.php
+++ htdocs/content/themes/tasks/app/views/tasks/all.scout.php
@@ -1,39 +1,48 @@
-<h2>Tasks:</h2>
+@extends('layouts.tasks')
 
-@if(isset($message) && $message)
-    <p>{{ $message }}</p>
-@endif
-
-<p>
-    <a href="{{ wp_nonce_url(home_url('tasks/create/'), 'create_task', 'action') }}">Create a task</a>
-</p>
-
-@if($query->have_posts())
+@section('pre-document')
     {{ Form::open('', 'post', false, array('nonce' => APP_NONCE, 'nonce_action' => 'task_remove')) }}
+@stop
+
+@section('toolbar')
+    <a href="{{ wp_nonce_url(home_url('tasks/create/'), 'create_task', 'action') }}" class="add-task">+ Add task</a>
+    <!-- Remove completed tasks -->
+    {{ Form::submit('remove', '- Remove', array('class' => 'remove-task')) }}
+@overwrite
+
+@section('messages')
+    @if(isset($message) && $message)
+        <p>{{ $message }}</p>
+    @endif
+@stop
+
+@section('content')
+    @if($query->have_posts())
         <ul>
             @while($query->have_posts())
-                <?php
-                    $query->the_post();
+            <?php
+            $query->the_post();
 
-                    // Get the due date.
-                    $dueDate = Meta::get(Loop::id(), 'task_due_date')
-                ?>
-                <li>
-                    <p>{{ Form::checkbox('task_check[]', Loop::id()) }}{{ Loop::title() }} | <a href="{{ wp_nonce_url(home_url('tasks/'.Loop::id().'/edit/'), 'edit_task', 'action') }}">Edit</a> - <a href="{{ wp_nonce_url(home_url('tasks/'.Loop::id().'/delete/'), 'delete_task', 'action') }}">Delete</a>
+            // Get the due date.
+            $dueDate = Meta::get(Loop::id(), 'task_due_date')
+            ?>
+            <li>
+                <p>{{ Form::checkbox('task_check[]', Loop::id()) }}{{ Loop::title() }} | <a href="{{ wp_nonce_url(home_url('tasks/'.Loop::id().'/edit/'), 'edit_task', 'action') }}">Edit</a> - <a href="{{ wp_nonce_url(home_url('tasks/'.Loop::id().'/delete/'), 'delete_task', 'action') }}">Delete</a>
                     @if(!empty($dueDate))
-                        | <b>{{{ $dueDate }}}</b>
+                    | <b>{{{ $dueDate }}}</b>
                     @endif
-                    </p>
-                </li>
+                </p>
+            </li>
             @endwhile
         </ul>
-        <footer>
-            {{ Form::submit('remove', 'Remove completed tasks') }}
-        </footer>
-    {{ Form::close() }}
-@endif
-<?php wp_reset_postdata(); ?>
+    @endif
+    <?php wp_reset_postdata(); ?>
 
-@if(!$query->have_posts())
-    <p>Yeah! No tasks for today. <a href="{{ wp_nonce_url(home_url('tasks/create/'), 'create_task', 'action') }}">Add a task ?</a></p>
-@endif
+    @if(!$query->have_posts())
+        <p>Yeah! No tasks for today. <a href="{{ wp_nonce_url(home_url('tasks/create/'), 'create_task', 'action') }}">Add a task ?</a></p>
+    @endif
+@stop
+
+@section('post-document')
+    {{ Form::close() }}
+@stop
